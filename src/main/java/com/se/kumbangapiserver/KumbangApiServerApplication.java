42bc0b8a9eac29dd84af081d4291f9045840d9ad
lichee55 <lichee55@gmail.com>
2022-04-19 17:06:22 +0900

    [Add] project files

---
+++ src/main/java/com/se/kumbangapiserver/KumbangApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.se.kumbangapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumbangApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KumbangApiServerApplication.class, args);
+    }
+
+}
